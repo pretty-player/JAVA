1f64f434df222980e36d415fe810310cd239972d
Silambarasan P <123256821+pretty-player@users.noreply.github.com>
2023-06-28 09:38:04 +0530

    Create Right.java

---
+++ Right.java
@@ -0,0 +1,14 @@
+//Right triangle
+class Right{
+
+public static void main(String []arg){
+
+  for(int i=1;i<=Integer.parseInt(arg[0]);i++){
+     for(int j=0;j<i;j++){
+       System.out.print("*");
+         } 
+  System.out.println();
+    }
+  }
+
+}
